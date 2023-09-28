75c41388411096d391b7e88c64ab48fa7faf1edc
Mohamad <m.ghaderi.bk@gmail.com>
2023-09-29 02:00:26 +0330

    listener for  event added

---
+++ app/Listeners/BadgeCheck.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\AchievementUnlocked;
+
+class BadgeCheck
+{
+    public function __construct()
+    {
+
+    }
+
+    public function handle(AchievementUnlocked $event): void
+    {
+
+    }
+}

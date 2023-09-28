0736ed945f67e5f4778775d52ebf73fddf2a6ded
Mohamad <m.ghaderi.bk@gmail.com>
2023-09-29 02:02:11 +0330

    listener for  event added

---
+++ app/Listeners/LessonAchievementCheck.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\LessonWatched;
+
+class LessonAchievementCheck
+{
+    public function __construct()
+    {
+
+    }
+
+    public function handle(LessonWatched $event): void
+    {
+
+    }
+}

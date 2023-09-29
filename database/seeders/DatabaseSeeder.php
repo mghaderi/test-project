eacf249500d9b2986180f80f8e624440ae476148
Mohamad <m.ghaderi.bk@gmail.com>
2023-09-29 07:18:03 +0330

    method run changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,13 +8,8 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $lessons = Lesson::factory()
-            ->count(20)
-            ->create();
+        $this->call(AssumptionSeeder::class);
     }
 }

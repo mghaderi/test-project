4a30bce1b496069873fcb783d8df186ec8fa6d53
Mohamad <m.ghaderi.bk@gmail.com>
2023-09-30 08:28:07 +0330

    method userBadgeReport added

--- app/Services/BadgeService.php
+++ app/Services/BadgeService.php
@@ -14,6 +14,34 @@ use Illuminate\Support\Facades\DB;
 
 class BadgeService
 {
+
+    public function userBadgeReport(User $user): array
+    {
+        $currentBadge = Badge::select('badges.*')
+            ->join('user_badges', 'badges.id', '=', 'user_badges.badge_id')
+            ->where('user_badges.user_id', $user->id)
+            ->orderByDesc('badges.minimum_achievement_amount')
+            ->first();
+        $defaultBadge = Badge::where('minimum_achievement_amount', 0)->first();
+        if (!$currentBadge instanceof Badge) {
+            $currentBadge = $defaultBadge;
+        }
+        $nextBadge = Badge
+            ::where('minimum_achievement_amount', '>', $currentBadge->minimum_achievement_amount)
+            ->orderBy('minimum_achievement_amount')
+            ->first();
+        $userAchievementsCount = UserAchievement::where('user_id', $user->id)->count();
+        $remaining = $nextBadge->minimum_achievement_amount ?? 0 - $userAchievementsCount;
+        if ($remaining < 0) {
+            $remaining = 0;
+        }
+        return [
+            'current_badge_name' => $currentBadge->name ?? '',
+            'next_badge_name' => $nextBadge->name ?? '',
+            'remaining_for_next_badge' => $remaining,
+        ];
+    }
+
     public function addToUserBadges(User $user): void
     {
         $badgeIdsOfUser = UserBadge::where('user_id', $user->id)
